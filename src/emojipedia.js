d6ba82039ccdb3ee7e91510f77931cf26de8fb16
Orhan ARI <66368354+ariorhan@users.noreply.github.com>
2022-05-30 13:43:08 +0100

    Initial commit

---
+++ src/emojipedia.js
@@ -0,0 +1,88 @@
+const emojipedia = [
+  {
+    id: 1,
+    emoji: "💪",
+    name: "Tense Biceps",
+    meaning:
+      "“You can do that!” or “I feel strong!” Arm with tense biceps. Also used in connection with doing sports, e.g. at the gym."
+  },
+  {
+    id: 2,
+    emoji: "🙏",
+    name: "Person With Folded Hands",
+    meaning:
+      "Two hands pressed together. Is currently very introverted, saying a prayer, or hoping for enlightenment. Is also used as a “high five” or to say thank you."
+  },
+  {
+    id: 3,
+    emoji: "🤣",
+    name: "Rolling On The Floor, Laughing",
+    meaning:
+      "This is funny! A smiley face, rolling on the floor, laughing. The face is laughing boundlessly. The emoji version of “rofl“. Stands for „rolling on the floor, laughing“."
+  },
+  {
+    id: 4,
+    emoji: "😂",
+    name: "Face with Tears of Joy",
+    meaning:
+      "A yellow face with a big grin, uplifted eyebrows, and smiling eyes, each shedding a tear from laughing so hard. Widely used to show something is funny or pleasing."
+  },
+  {
+    id: 5,
+    emoji: "🥰 ",
+    name: "Smiling Face with Hearts",
+    meaning:
+      "A yellow face with smiling eyes, a closed smile, rosy cheeks, and several hearts floating around its head. Expresses a range of happy, affectionate feelings, especially being in love. The placement of the hearts varies slightly by platform."
+  },
+  {
+    id: 6,
+    emoji: "🤫",
+    name: "Shushing Face",
+    meaning:
+      "A yellow face placing an index finger over pursed lips, as if issuing Shh! or Shush! for silence. Some platforms feature raised eyebrows. Apple’s design suggests the face is tilting back slightly. "
+  },
+  {
+    id: 7,
+    emoji: "🙄 ",
+    name: "Face with Rolling Eyes",
+    meaning:
+      "A yellow face with a small, closed mouth, flat or frowning, rolling its large, white eyes upwards. As with the gesture of an eye-roll, commonly conveys moderate disdain, disapproval, frustration, or boredom. Tone varies, including playful, sassy, resentful, and sarcastic, as if saying Yeah, whatever."
+  },
+  {
+    id: 8,
+    emoji: "😡 ",
+    name: "Enraged Face",
+    meaning:
+      "A red face with an angry expression: frowning mouth with eyes and eyebrows scrunched downward. Bears the same expression as 😠 Angry Face on most platforms and may convey more intense degrees of anger, e.g., hate or rage."
+  },
+  {
+    id: 9,
+    emoji: "🙉 ",
+    name: "Hear-No-Evil Monkey",
+    meaning:
+      "The hear no evil monkey, called Kikazaru (Japanese for “hear not”), one of the Three Wise Monkeys. Depicted as the brown 🐵 Monkey Face with hands covering its ears."
+  },
+  {
+    id: 10,
+    emoji: "🐮",
+    name: "Cow Face",
+    meaning:
+      "A friendly, cartoon-styled face of a cow looking straight ahead. Variously depicted as a white, brown, or multicolored cow face with a neutral expression, short horns, and a pink or brown muzzle."
+  },
+  {
+    id: 11,
+    emoji: "🐹",
+    name: "Hamster",
+    meaning:
+      "A friendly, cartoon-styled face of a hamster—a small rodent often kept as a pet—looking straight ahead. Generally depicted as a brown-and-white faced hamster with whiskers, a pink nose, puffy cheeks, and large, round, pink ears, sometimes with a buck tooth."
+  },
+  {
+    id: 12,
+    emoji: "🔥",
+    name: "Fire",
+    meaning:
+      "A flame, as produced when something is on fire. Depicted as a red, orange, and yellow flickering flame."
+  }
+];
+
+export default emojipedia;
